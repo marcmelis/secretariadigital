b6ede210f5c536ef22baf89a403104096c6b2080
marcmelis <marc.melis@hayanmind.com>
2023-04-20 18:15:16 +0200

    readd variables

--- app.py
+++ app.py
@@ -2,7 +2,11 @@ from flask import Flask, request, jsonify
 
 # Import your existing functions and variables
 from assistant import (
-    answer_query_with_context, get_embeddings, get_df
+    answer_query_with_context, get_embeddings, get_df,
+    CONFIG_FILE, EMBEDDINGS_FILE, DEFAULT_LANGUAGE, API_KEY, COMPLETIONS_MODEL,
+    COMPLETIONS_API_PARAMS, MAX_SECTION_LEN, SEPARATOR, tokenizer, separator_len,
+    get_query_embedding, order_document_sections_by_query_similarity,
+    construct_prompt, generate_chat_completion_message_from_prompt
 )
 
 app = Flask(__name__)
